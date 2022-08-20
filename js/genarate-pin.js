027daace10a8d80d3f8840f704cb426b3240f59e
Spprince007 <spprince007@gmail.com>
2022-08-21 01:33:43 +0600

    otp done

--- js/genarate-pin.js
+++ js/genarate-pin.js
@@ -20,3 +20,47 @@ document.getElementById('generate-pin').addEventListener('click', function () {
     const displayPin = document.getElementById('display-pin');
     displayPin.value = pin;
 })
+
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const typeNumberField = document.getElementById('typed-numbers');
+    const previousNumber = typeNumberField.value;
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typeNumberField.value = '';
+            // console.log('its working');
+        }
+        else if (number === '<') {
+            const digits = previousNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typeNumberField.value = remainingDigits;
+        }
+    }
+
+    else {
+
+        const newTypedNumber = previousNumber + number
+        typeNumberField.value = newTypedNumber;
+    }
+})
+
+document.getElementById('verify-pin').addEventListener('click', function () {
+    const displyPin = document.getElementById('display-pin');
+    const displayedPin = displyPin.value;
+
+    const typedNumber = document.getElementById('typed-numbers');
+    const inputPin = typedNumber.value;
+
+    let message = document.getElementById('pin-success');
+    let warring = document.getElementById('pin-failure')
+    if (displayedPin === inputPin) {
+        message.style.display = 'block';
+        warring.style.display = 'none';
+        // console.log('its working');
+    }
+    else {
+        warring.style.display = 'block';
+        message.style.display = 'none';
+    }
+})
